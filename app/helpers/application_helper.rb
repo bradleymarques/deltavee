6b839e8577437018166d622598d88d350363c3ed
Bradley Marques <brad@next45.co>
2021-05-09 21:26:20 +0200

    Align flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def toast_klass(flash_message_klass)
     case flash_message_klass.to_sym
-    when :alert
-      "toast align-items-center bg-danger border-0"
+    when :alert, :error
+      "toast align-items-center bg-danger border-0 mb-2"
     else
-      "toast align-items-center bg-primary border-0"
+      "toast align-items-center bg-primary border-0 mb-2"
     end
   end
 end
