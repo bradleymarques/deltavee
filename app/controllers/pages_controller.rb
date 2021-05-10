73b764147e8f81fcee4af7be5bab6c1c5fe82ee6
Bradley Marques <brad@next45.co>
2021-05-10 18:50:39 +0200

    Fixes controller tests

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,9 @@ class PagesController < ApplicationController
   def home
   end
 
+  def about
+  end
+
   private
 
   def check_signed_in
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get "dashboard", to: "dashboards#show"
+
+  get "about", to: "pages#about"
+  get "home", to: "pages#home"
+
   root to: "pages#home"
 end
