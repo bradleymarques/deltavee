f1fe16a77d2d285f670ada1e9e13f746bd483451
Bradley Marques <brad@next45.co>
2021-05-11 17:50:38 +0200

    Fixes failing tests

--- test/system/pages_test.rb
+++ test/system/pages_test.rb
@@ -7,16 +7,6 @@ class PagesTest < ApplicationSystemTestCase
     assert_selector("h1", text: I18n.t("app.app_name"))
   end
 
-  test "a user can visit the root path and see the app's long name" do
-    visit(root_path)
-    assert_selector("h2", text: I18n.t("app.app_name_long"))
-  end
-
-  test "a user can visit the root path and see the app's description" do
-    visit(root_path)
-    assert_selector("p", text: I18n.t("home.app_description"))
-  end
-
   test "a user can visit the root path and see a link to login" do
     visit(root_path)
     assert_selector("a", text: I18n.t("navigation.login"))
