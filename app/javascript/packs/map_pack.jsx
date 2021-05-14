3ba2835cd5e93b83c639efeb0da11a1873130e7c
Bradley Marques <brad@next45.co>
2021-05-14 19:44:11 +0200

    Tries to use some typescript

--- app/javascript/packs/map_pack.jsx
+++ app/javascript/packs/map_pack.jsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import UniverseMap from "map/universe_map";
 
 document.addEventListener("turbolinks:load", () => {
   const universeMapContainer = document.getElementById("universe-map-container")
 
   if (universeMapContainer) {
     ReactDOM.render(
-      <p>The Map</p>,
+      <UniverseMap />,
       universeMapContainer,
     );
   }
