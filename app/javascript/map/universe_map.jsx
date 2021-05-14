bb3b67c55b57e67bdfc4927eb506b12d3f019a42
Bradley Marques <brad@next45.co>
2021-05-14 19:45:52 +0200

    Corrects export

---
+++ app/javascript/map/universe_map.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const UniverseMap = (props) => {
+  return <p>universe map</p>
+};
+
+export default UniverseMap;
--- app/javascript/map/universe_map.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-interface Props {
-
-}
-
-const UniverseMap: React.FC<Props> = (props) => {
-  return <p>universe map</p>
-};
