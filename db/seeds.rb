09892366c7127cf7aafb8a02bc8132cd0aff87b7
Bradley Marques <brad@next45.co>
2021-05-15 09:08:13 +0200

    Corrects filename

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-filename = Rails.root.join("data", "stars.csv")
-
+# Import Systems from CSV file
 importer = SystemCsvImporter.new(
-  filename: filename,
+  filename: Rails.root.join("data", "systems.csv"),
   row_count: 1000,
   show_progress: true
 )
