3766762a431e391c70ef15296710645befb44a7a
Bradley Marques <brad@next45.co>
2021-05-15 21:12:51 +0200

    Tests .nav_link_klass

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -14,4 +14,18 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal("toast align-items-center bg-danger border-0", toast_klass("alert"))
     assert_equal("toast align-items-center bg-primary border-0", toast_klass("something-else"))
   end
+
+  test "#nav_link_klass returns an active nav-link if the current_page is one
+        of the paths" do
+    stubs("current_page?").returns(true)
+    paths = ["path/1", "path/2"]
+    assert_equal("nav-link active", nav_link_klass(paths))
+  end
+
+  test "#nav_link_klass returns an inactive nav-link if the current_page is one
+        of the paths" do
+    stubs("current_page?").returns(false)
+    paths = ["path/1", "path/2"]
+    assert_equal("nav-link", nav_link_klass(paths))
+  end
 end
