263d6d6ebc9891339cd5d063fe4b6acedf3f509b
Bradley Marques <brad@next45.co>
2021-05-18 09:03:29 +0200

    Can mouse over system to turn it green

--- app/javascript/maps/system.tsx
+++ app/javascript/maps/system.tsx
@@ -10,9 +10,18 @@ interface SystemProps {
 const System: React.FC<SystemProps> = (props) => {
   const { name, x, y, z } = props;
 
+  const mesh = React.useRef();
+
+  const [hovered, setHovered] = React.useState(false);
+
   return (
-    <mesh position={[x, y, z]}>
+    <mesh
+      ref={mesh}
+      position={[x, y, z]}
+      onPointerOver={(event) => setHovered(true)}
+      onPointerOut={(event) => setHovered(false)}>
       <sphereGeometry args={[0.1, 10, 10]}/>
+      <meshStandardMaterial color={ hovered ? 'green' : 'white' } />
     </mesh>
   );
 };
